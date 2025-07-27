6f76b4b4f5cfca7ad0cee7d002a69c00798dfe6b
bogere moses <b.bogere@servicecops.com>
2025-07-27 23:35:51 +0300

    testing the employment act

--- backend/app/services/stt.py
+++ backend/app/services/stt.py
@@ -5,5 +5,6 @@ headers = {"authorization": ASSEMBLYAI_API_KEY}
 
 def transcribe_audio_url(audio_url: str):
     endpoint = "https://api.assemblyai.com/v2/transcript"
-    response = requests.post(endpoint, json={"audio_url": audio_url}, headers=headers)
+    model = "universal"
+    response = requests.post(endpoint, json={"audio_url": audio_url, "speech_model": model}, headers=headers)
     return response.json()
